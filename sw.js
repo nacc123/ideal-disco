9011776c3701f416726ea03edc0da9165faa2a43
nacc123 <37048565+nacc123@users.noreply.github.com>
2026-08-24 13:09:02 +0200

    Refresh PWA cache for station map V11.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="chargecompare-v11-5";
-const APP=["./","./index.html","./styles.css","./app.js","./garage-sync.js","./waze.js","./manifest.json","./icon.svg"];
+const CACHE="chargecompare-v11-6";
+const APP=["./","./index.html","./styles.css","./app.js","./garage-sync.js","./waze.js","./station-map-ui.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
